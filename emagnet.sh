584e988d50ced3624d92f28d11b2a37c8b396a25
wuseman <wuseman@nr1.nu>
2020-05-20 05:32:02 +0100

    fixed --banned feature

--- emagnet.sh
+++ emagnet.sh
@@ -290,11 +290,13 @@ fi
 #### Before we trying to scrape pastebin we wanna know if we are banned or not#
 ###############################################################################
 emagnet_wasibanned() {
-	curl -sL "$USERAGENT" "$PASTEBIN"|grep -q "blocked your IP"
+	emagnet_required_stuff
+    emagnet_conf
+    curl -sL "$USERAGENT" "$PASTEBIN"|grep -q "blocked your IP"
         if [[ $? -eq "0" ]]; then
-          echo -e "$basename$0: $(curl -s $WIP) is blocked, turn on your vpn or wait ~30 minutes ..."
+          echo -e "Please wait...";echo -e "$basename$0 \e[1;31m$(curl -s $WIP2)\e[0m is blocked, turn on your vpn or wait ~30 minutes ..."
         else
-          echo -e "$basename$0: $(curl -s $WIP) has not been blocked ..."
+          echo -e "Please wait...";echo -e "$basename$0: \e[1;31m$(curl -s $WIP2)\e[0m has not been blocked ..."
         fi
 }
 
