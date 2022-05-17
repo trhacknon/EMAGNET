af5e3cdd787ea307a83c5f885e81bc99e074944f
wuseman <wuseman@nr1.nu>
2022-05-17 12:28:46 +0200

    added done msg

--- emagnet_beta.sh
+++ emagnet_beta.sh
@@ -381,10 +381,10 @@ emagnet_screen() {
 
 
 emagnet_main() {
-   seq 1| xargs  -I % -n1 -P20|curl -sL ${sqURL}|sed 's/\\//g'|grep -Eo "(http|https)://www.[a-zA-Z0-9./?=_-]*"|sed 's/u0022//g'  > 1
-   xargs -n 1 -P 18 curl -sL -u "${lOGIN}" -H "${uGENT}" --resolve ${sHOST}:443:${lHOST} < 1 |grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*download.*txt" > 2
+   seq 1| xargs  -I % -n1 -P20|curl -sL ${sqURL}|sed 's/\\//g'|grep -Eo "(http|https)://www.[a-zA-Z0-9./?=_-]*"|sed 's/u0022//g'  > ${tPATH}/urls1.txt
+   xargs -n 1 -P 18 curl -sL -u "${lOGIN}" -H "${uGENT}" --resolve ${sHOST}:443:${lHOST} < ${tPATH}/urls1.txt |grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*download.*txt" > ${tPATH}/urls2.txt
     xargs -P "$(xargs --show-limits -s 1 2>&1|grep -i "parallelism"|awk '{print $8}')" -n 1 \
-        wget --no-check-certificate --user-agent="${USERAGENT}" -q -nc -P "$dPATH" < 2
+        wget --no-check-certificate --user-agent="${USERAGENT}" -q -nc -P "$dPATH" < ${tPATH}/urls2.txt
 }
 
 
@@ -398,8 +398,8 @@ function lets_pwn() {
     folder_check
     #grab_urls_source
     emagnet_main
+    echo -e "Done!"
 
-    echo -e "\n=========================================================================\n"
 }
 
 OPTIND=1
